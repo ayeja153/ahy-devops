1229b164c21bce0f5edc2223009104655d3b8069
root <root@compilation.uci.cu>
2023-05-16 14:40:26 -0400

    First lines of project

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/factorial/<numb>')
+def calculate_factorial(numb):
+    num = int(numb)
+    factorial = 1    
+    if num < 0:    
+       return 'Factorial does not exist for negative numbers'    
+    elif num == 0:    
+       return 'The factorial of 0 is 1'    
+    else:    
+       for i in range(1,num + 1):    
+           factorial = factorial*i    
+       return f'The factorial of {num} is {factorial}'
+
+if __name__ == "__main__":
+    app.run()
